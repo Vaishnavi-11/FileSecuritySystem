933535d513f98f42596dcf444747205e5ea374c8
Tejaswini V <67691978+Tejaswini-1502@users.noreply.github.com>
2021-07-26 16:34:51 +0530

    Update inpForEncryption.sh

--- inpForEncryption.sh
+++ inpForEncryption.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "Enter the absolute path of the file:"
 read path
-password=""
-echo "Enter the password"
-read password
-echo "$path;$password" >> t1.txt
-#t1.txt is a text file which holds the pathname of the file to be encrypted and the user password separated by a semicolon.
+if [ -f "$path" ]
+then
+	echo "Enter the password"
+	read password
+	echo "$path;$password" >> t1.txt
+	#t1.txt is a text file which holds the pathname of the file to be encrypted and the user password 		separated by a semicolon.
+else
+	echo "$path does not exists"
+
+fi
