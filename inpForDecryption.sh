97570ed5d32713a45dd594cf5ccc4a639d4d1eb2
Tejaswini V <67691978+Tejaswini-1502@users.noreply.github.com>
2021-07-26 23:08:04 +0530

    Update inpForDecryption.sh

--- inpForDecryption.sh
+++ inpForDecryption.sh
@@ -5,11 +5,19 @@ read pathname
 
 if [ -f "$pathname" ]
 then
-	
-	if [ grep -wc "$pathname" t1.txt == 1 ]
+	count=$( grep -c "$pathname" t1.txt )
+	if [ $count == 1 ]
 	then
 		echo "Enter Password"
 		read password
+		valid=$( grep -c "$pathname_$password" t1.txt)
+		if [ $valid == 1 ]
+		then
+			#decryption of the file
+			echo "Your file has been decrypted"
+		else
+			echo "Invalid password"
+		fi
 	else
 		echo "Incorrect Pathname of the file"
 	fi
