d109c1bdc6650d2eda96eb8cf2773ed7344ebed8
YashasviManjunath <87889429+YashasviManjunath@users.noreply.github.com>
2021-07-28 14:39:44 +0530

    Add files via upload

---
+++ getstarted.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+
+if zenity --info --window-icon='/home/yash/Downloads/smileb1.jpg'  --title="Get started" --text="Click on OK to GET STARTED" --height=200 --width=500
+then
+  sh enorde.sh
+fi
